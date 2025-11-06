ec03d015b6efb7830bca290173d8f1beb6141e8b
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-11-06 13:57:04 +0600

    - add two way stream

--- client.ts
+++ client.ts
@@ -15,3 +15,30 @@ client.SayHello({ name: "Ashik" }, (err: any, res: any) => {
   if (err) return console.error(err);
   console.log("🎉 Response from server:", res.message);
 });
+
+// stream from the server
+const sfs = client.SayHelloStream({ name: "Ashik" });
+
+sfs.on("data", (response: any) => {
+  console.log("📩 Stream message:", response.message);
+});
+
+sfs.on("end", () => {
+  console.log("✅ Stream ended");
+});
+
+sfs.on("error", (err: any) => {
+  console.error("❌ Stream error", err);
+});
+
+// stream to the server
+const s2s = client.SendNamesStream((err: any, res: any) => {
+  if (err) return console.error("❌ Server error:", err);
+  console.log("🎉 Server response:", res.message);
+});
+
+["Ashik", "Sarkar", "BunJS"].forEach((name) => {
+  s2s.write({ name });
+});
+
+s2s.end();
--- server.ts
+++ server.ts
@@ -13,6 +13,39 @@ server.addService(greeter.Greeter.service, {
     console.log(call.request);
     callback(null, { message: `Hello, ${call.request.name}` });
   },
+  // stream to the client
+  SayHelloStream: (call: any) => {
+    const { name } = call.request;
+
+    let count = 0;
+    const interval = setInterval(() => {
+      count++;
+      call.write({ message: `Hello ${name}, message #${count}` });
+
+      if (count >= 5) {
+        clearInterval(interval);
+        call.end();
+      }
+    }, 1000);
+  },
+  // stream from the client
+  SendNamesStream: (call: any, callback: any) => {
+    const names: string[] = [];
+
+    call.on("data", (req: any) => {
+      console.log("📥 Received:", req.name);
+      names.push(req.name);
+    });
+
+    call.on("end", () => {
+      const message = `Hello to ${names.join(", ")}`;
+      callback(null, { message });
+    });
+
+    call.on("error", (err: any) => {
+      console.error("❌ Stream error:", err);
+    });
+  },
 });
 
 server.bindAsync(
